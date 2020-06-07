450dee626f589fb0d00b24870d9f3f07e2888a02
Jaya-272009 <jayagaur911@gmail.com>
2020-06-07 17:42:12 +0530

    added cart reducer and function to avoid duplicate items and increase quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux';
 import './cart-icon.styles.scss';
-import toggleCartHidden from '../../redux/cart/cart.actions';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
+
 
 const CartIcon = ({toggleCartHidden})=>(
     <div className="cart-icon" onClick={toggleCartHidden}>
