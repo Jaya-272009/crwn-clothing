434363b1b25ac8ccf40b3af8d05f49e57f860874
Jaya-272009 <jayagaur911@gmail.com>
2020-06-08 13:14:50 +0530

    added selector for cartitems

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -27,4 +27,5 @@ const CollectionItem = ({item,addItem})=>{
  const mapDispatchToProps = dispatch =>({
      addItem:item => dispatch(addItem(item))
  })
+ 
 export default connect(null, mapDispatchToProps)(CollectionItem);
