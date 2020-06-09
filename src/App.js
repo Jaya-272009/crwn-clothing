f1e82ae2afa6075cba4033e28b55989c63498dc0
Jaya-272009 <jayagaur911@gmail.com>
2020-06-09 15:48:14 +0530

    created checkout page and added persist to store cart reducer in localstorage

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import {auth,createUserProfileDocument} from './firebase/firebase.utils'
+import checkoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+
+import {auth,createUserProfileDocument} from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions';
-
-
+import {createStructuredSelector} from 'reselect'
 
 
 class App  extends React.Component {
@@ -50,13 +52,14 @@ class App  extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
           <Route exact path="/signin" render={()=> this.props.currentUser? (<Redirect to='/' /> ): (<SignInAndSignUpPage />)} />
+          <Route exact path="/checkout" component={checkoutPage} />
       </Switch>
  </div>
   );
   }
 }
-const mapStateToProps = ({user}) =>({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: setCurrentUser
 }) 
 
 const mapDispatchToProps = dispatch =>({
